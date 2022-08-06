58997ab8b6cd8462775458508f6bf428e4bb875c
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-08-06 15:33:59 -0700

    Time: 97 ms (82.13%), Space: 42.1 MB (98.60%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -10,12 +10,9 @@ var isAnagram = function(s, t) {
     if(s.length!=t.length){
         return false;
     }
-    for(var i=0;i<s.length;i++){
-        //console.log(" s[i]"+s[i]);
-        alphabet[s[i].charCodeAt()-97]+=1;
-       // console.log(" alphabet[s[i]] "+alphabet[s[i].charCodeAt()-97]);
-        alphabet[t[i].charCodeAt()-97]-=1;
-       // console.log(" alphabet[t[i]] "+alphabet[t[i].charCodeAt()-97]);
+    for(var i=0;i<s.length;i++){        
+        alphabet[s[i].charCodeAt()-97]+=1;     
+        alphabet[t[i].charCodeAt()-97]-=1;       
     }
     
     for( var i=0;i<26;i++){
