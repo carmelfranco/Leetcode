f582b6d6b86d8fc73464062f2f33dcf2715c5a9f
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-08-06 16:04:46 -0700

    Time: 225 ms (23.54%), Space: 50.7 MB (68.97%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,7 +3,7 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    const map1= new Map();
+   /* const map1= new Map();
     for(var i=0;i<nums.length;i++){
         var b=nums[i];
         if(map1.has(b)){
@@ -12,5 +12,13 @@ var containsDuplicate = function(nums) {
         map1.set(b,i);
     }
     
+    return false;*/
+    nums.sort(function(a, b){return a - b});
+    
+    for( var i=1;i<nums.length;i++){
+        if(nums[i]==nums[i-1]){
+            return true;
+        }
+    }
     return false;
 };
