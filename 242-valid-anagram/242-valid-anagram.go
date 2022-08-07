0ac9b199aa9e3db0a98986e83db779dfb038f029
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-08-06 20:37:15 -0700

    Time: 7 ms (67.15%), Space: 3.7 MB (11.69%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.go
+++ 242-valid-anagram/242-valid-anagram.go
@@ -21,7 +21,7 @@
     return true
 }*/
 
-/* Using Sort , Time Complexity of n log n and Space complexity of O(1) */
+/* Using Sort , Time Complexity of n log n and Space complexity of O(1) 
 import ("strings"
         "sort")
 func SortRuneSlice( w string) []rune{
@@ -52,4 +52,27 @@ func isAnagram(s string, t string) bool {
   
     return true
 }
+*/
 
+/* using Time Complexity O(n) and Space complexity O(26) */
+
+func isAnagram(s string, t string) bool {
+    if len(s)!=len(t) {
+        return false
+    }
+    var alphabet[26] int 
+    runes1 := []rune(s)
+    runes2 := []rune(t)
+    for i := 0; i < len(runes1); i++ {
+		alphabet[int(runes1[i])-97]++
+        alphabet[int(runes2[i])-97]--
+	}
+    
+    for i:=0;i<26;i++ {
+        if alphabet[i]!=0 {
+            return false
+        }
+    }
+    
+    return true
+}
