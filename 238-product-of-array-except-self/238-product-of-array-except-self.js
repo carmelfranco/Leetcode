51e304541029e0609c9b2c6d585a9332a6590654
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-08-09 20:54:17 -0700

    Time: 155 ms (55.11%), Space: 58.1 MB (6.01%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -2,6 +2,8 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+
+/* Time complexity O(n)**2 Space complexity O(n)
 var productExceptSelf = function(nums) {
     let res=Array(nums.length);
    for(let i=0;i<nums.length;i++){
@@ -15,5 +17,27 @@ var productExceptSelf = function(nums) {
        res[i]=temp;
    }
     
+    return res;
+};*/
+
+var productExceptSelf = function(nums) {
+   let res=Array(nums.length);
+   let prefix=Array(nums.length).fill(1);
+   let postfix=Array(nums.length).fill(1);
+   prefix[0]=nums[0];
+   for(let i=1;i<nums.length;i++){
+       prefix[i]=prefix[i-1]*nums[i];
+   }
+   //console.log(" Prefix :"+prefix);
+   postfix[nums.length-1]=nums[nums.length-1];
+   for(let i=nums.length-1;i>=1;i--){
+       postfix[i-1]=postfix[i]*nums[i-1];
+   }
+   //console.log(" postfix :"+postfix);
+   res[0]=1*postfix[1];
+   for(let i=1;i<nums.length-1;i++){
+       res[i]=prefix[i-1]*postfix[i+1];
+   }
+   res[nums.length-1] = prefix[nums.length-2];
     return res;
 };
