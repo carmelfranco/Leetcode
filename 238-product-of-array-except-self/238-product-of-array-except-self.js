6fed2febf8ee7aec002cb06da8dd0961dab88e8e
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-08-09 20:25:03 -0700

    Time: 9567 ms (5.01%), Space: 52.4 MB (94.85%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -3,17 +3,17 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    let res=Array(nums.length).fill(1);
-    let prefix=1;
-    for(let i=0;i<nums.length;i++){
-        res[i]=prefix;
-        prefix*=nums[i];
-    }
-    let postfix=1;
-    for(let i=nums.length-1;i>=0;i--){
-        res[i] *= postfix;
-        postfix*=nums[i];
-    }
+    let res=Array(nums.length);
+   for(let i=0;i<nums.length;i++){
+       var temp=1;
+       for( let j=0;j<nums.length;j++){
+           if(i!=j){
+                temp*=nums[j];
+           }
+          
+       }
+       res[i]=temp;
+   }
     
     return res;
 };
