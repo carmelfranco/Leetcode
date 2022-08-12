ee3dc421e35e08e4fe57680986c218a99a0dda87
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-08-11 23:57:42 -0700

    Time: 135 ms (34.53%), Space: 45.2 MB (62.77%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.js
+++ 36-valid-sudoku/36-valid-sudoku.js
@@ -3,27 +3,25 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
+    let set= new Set();
     
-    let set = new Set()
-    
-    for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board[0].length; j++) {
-            const value = board[i][j]
-            if (value !== ".") {
-                const row = `${value} at row ${i}`
-                const column = `${value} at column ${j}`
-                const box = `${value} at box ${Math.floor(i/3)}, ${Math.floor(j/3)}`
-                //console.log(`${row}, ${column}, ${box}`)
-                if (set.has(row) || set.has(column) || set.has(box)) {
-                    return false
-                } else {
-                    set.add(row)
-                    set.add(column)
-                    set.add(box)
+    for(let i=0;i<board.length;i++){
+        for(let j=0;j<board[0].length;j++){
+            const value=board[i][j];
+            if(value!="."){
+                const row=`${value} at row ${i}`;
+                const col=`${value} at col ${j}`;
+                const box=`${value} at box ${Math.floor(i/3)}, ${Math.floor(j/3)}`;
+                
+                if(set.has(row) || set.has(col) || set.has(box)){
+                    return false;
+                }else{
+                    set.add(row);
+                    set.add(col);
+                    set.add(box);
                 }
             }
         }
     }
-
-    return true
+  return true;
 };
