e6c022754020e9e9372ed8ff6a2228964a145e08
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-08-15 23:00:49 -0700

    Time: 115 ms (51.03%), Space: 49.9 MB (38.93%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.js
+++ 11-container-with-most-water/11-container-with-most-water.js
@@ -2,24 +2,43 @@
  * @param {number[]} height
  * @return {number}
  */
+
+/* Time Complexity O(n*n) , space complexity O(1)
 var maxArea = function(height) {
-    let area=0;    
+    let area=0;
+    let res=0;
+    for(let i=0;i<height.length;i++){
+        for(let j=i+1;j<height.length;j++){
+            area=(j-i)*Math.min(height[i],height[j]);
+            
+            if(res<area){
+                res=area;
+            }
+        }
+    }
+    
+    return res;
+};*/
+
+/* Time complexity O(n) space complexity O(1) */
+var maxArea = function(height) {
+    let area=0;
     let res=0;
     let i=0;
     let j=height.length-1;
+    
     while(i<j){
-        area=Math.min(height[i],height[j])*(j-i);
+        area=(j-i)*Math.min(height[i],height[j]);
         if(res<area){
             res=area;
         }
+        
         if(height[i]<=height[j]){
-            i++;
+            i++
         }else{
             j--;
         }
     }
-    
-    
-    
+   
     return res;
 };
