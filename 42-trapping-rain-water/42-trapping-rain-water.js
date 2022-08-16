b48445eb79601e0a99636b6ad5464cefe382ef0e
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-08-15 23:58:14 -0700

    Time: 145 ms (14.72%), Space: 43.2 MB (51.01%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.js
+++ 42-trapping-rain-water/42-trapping-rain-water.js
@@ -2,10 +2,10 @@
  * @param {number[]} height
  * @return {number}
  */
+/* Time Complexity O(n) , Space Complexity O(n) 
 var trap = function(height) {
     let maxleft=Array(height.length);
-    let maxright=Array(height.length);
-    let minarr=Array(height.length);
+    let maxright=Array(height.length);    
     let res=0;
     maxleft[0]=height[0];
     for(let i=1;i<height.length;i++){
@@ -20,4 +20,26 @@ var trap = function(height) {
     }
     
     return res;
-};
+};*/
+
+var trap = function(height) {
+    let leftmax=height[0];
+    let rightmax=height[height.length-1];
+    let i=0;
+    let j=height.length-1;
+    let res=0;
+    
+    while(i<j){
+        if(leftmax<=rightmax){
+            i++;
+            leftmax=Math.max(leftmax,height[i]);
+            res+=leftmax-height[i];
+        }else{
+            j--;
+            rightmax=Math.max(rightmax,height[j]);
+            res+=rightmax-height[j];
+        }
+    }
+    
+    return res;
+}
