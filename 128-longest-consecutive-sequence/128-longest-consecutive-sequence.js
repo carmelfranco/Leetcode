b7047947c4e45c67ea4f445d273adb8759b52865
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-08-26 22:44:58 -0700

    Time: 877 ms (6.20%), Space: 57.8 MB (58.12%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
@@ -2,20 +2,55 @@
  * @param {number[]} nums
  * @return {number}
  */
-var longestConsecutive = function(nums) {
-    let s1= new Set(nums);
-    let longest =0;
-    console.log(s1);
-    for(let n of nums){        
-        if(s1.has(n-1)==false){
-            let len = 0;
-            while(s1.has(n+len)){
-                
-                len+=1;
-            }
-            longest=longest>len?longest:len;
+
+// O n Log (n), space complexity O(n)
+/*var longestConsecutive = function(nums) {
+    if(nums.length==0){
+        return 0;
+    }
+    if(nums.length==1){
+        return 1;
+    }
+    nums=nums.sort((a,b)=>(a-b));
+    let uniqueints = nums.filter((c, index) => {
+    return nums.indexOf(c) === index;
+});
+    
+    if(uniqueints.length==1){
+        return 1;
+    }
+    let longest=0;
+    let tcount=1;
+    //console.log(nums);
+    for(let i=1;i<uniqueints.length;i++){
+        if(uniqueints[i]==uniqueints[i-1]+1 ){
+            tcount++;
+            longest=Math.max(longest,tcount);
+        }else{
+            tcount=1;
         }
+        
+        
+    }
+    if(longest==0){
+        return 1;
+    }
+    return longest;
+};*/
+
+var longestConsecutive = function(nums) {
+    let set1= new Set(nums);
+    let longest=0;
+    for( n of nums){
+        if(set1.has(n-1)==false){
+            let len=0;
+            while(set1.has(n+len)){
+                len++;
+            }
+            longest=Math.max(longest,len);
+        }
+        
     }
     
     return longest;
-};
+}
