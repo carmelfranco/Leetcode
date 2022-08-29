bdca5cdcc5b36d6e2dd025d2f379a6cc19e8b0cb
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-08-28 22:17:58 -0700

    Time: 97 ms (83.98%), Space: 41.6 MB (99.73%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.js
@@ -4,17 +4,18 @@
  * @return {number}
  */
 var characterReplacement = function(s, k) {
-    let left=0,maxf=0,output=0;
+    let left=0,res=0,maxf=0;
     let chars={};
     for(let right=0;right<s.length;right++){
         let char=s[right];
         chars[char]=1+(chars[char]||0);
         maxf=Math.max(maxf,chars[char]);
         while((right-left+1)-maxf>k){
-            chars[s[left]]-=1;
+            chars[s[left]]--;
             left++;
         }
-        output=Math.max(output,right-left+1);
+        res=Math.max(res,right-left+1);
     }
-    return output;
+    
+    return res;
 };
