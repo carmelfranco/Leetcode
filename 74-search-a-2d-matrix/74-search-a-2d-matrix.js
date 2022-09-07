6f877f1c417016c794b0990038126440a7da3cc5
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-09-07 01:00:38 -0700

    Time: 108 ms (18.82%), Space: 41.9 MB (80.25%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.js
@@ -4,8 +4,8 @@
  * @return {boolean}
  */
 
-// Time Complexity O(log n), space complexity O(n)
-var searchMatrix = function(matrix, target) {
+// Time Complexity O(log n), space complexity O(n*m)
+/*var searchMatrix = function(matrix, target) {
     let temp=matrix.join(",").split(",");
     let i=0, j=temp.length-1;
     while(i<=j){
@@ -19,4 +19,37 @@ var searchMatrix = function(matrix, target) {
         }
     }
     return false;
-};
+};*/
+
+var searchMatrix = function(matrix, target) {
+    let left=0, right=matrix[0].length-1;
+    let top=0, bot=matrix.length-1;
+    
+    while(top<=bot){
+        let row=Math.trunc((top+bot)/2);
+        if(target<matrix[row][0]){
+            bot=row-1;
+        }else if(target>matrix[row][right]){
+            top=row+1;
+        }else{
+            break;
+        }
+    }
+    
+    if(top>bot){
+        return false;
+    }
+    let row=Math.trunc((top+bot)/2);
+    while(left<=right){
+        let mid=Math.trunc((left+right)/2);
+        if(target<matrix[row][mid]){
+            right=mid-1;
+        }else if(target>matrix[row][mid]){
+            left=mid+1;
+        }else{
+            return true;
+        }
+    }
+    
+    return false;
+}
