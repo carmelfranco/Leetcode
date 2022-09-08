ab38a6377531f8b15154882c91602f6042be328e
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-09-07 23:18:43 -0700

    Time: 97 ms (37.80%), Space: 42.1 MB (67.80%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -3,8 +3,35 @@
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {
+
+// Time Complexity O(n)
+/*var search = function(nums, target) {
     let eq=(element)=>(element==target);
     
     return nums.findIndex(eq);
-};
+};*/
+
+var search = function(nums, target) {
+    let i=0, j=nums.length-1;
+    while(i<=j){
+        let mid=Math.trunc((i+j)/2);
+        if(nums[mid]==target){
+            return mid;
+        }
+        
+        if(nums[i]<=nums[mid]){ // Left Portion
+            if(target>nums[mid] || target<nums[i]){
+                i=mid+1;
+            }else{
+                j=mid-1;
+            }
+        }else{ // Right Portion
+            if(target<nums[mid] || target>nums[j]){
+                j=mid-1;
+            }else{
+                i=mid+1;
+            }
+        }
+    }
+    return -1;
+}
