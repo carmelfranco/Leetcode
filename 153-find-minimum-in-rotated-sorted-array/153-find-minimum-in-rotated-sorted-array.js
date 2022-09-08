358ad6d6c25a8af450af9cb3775a2b89d0a5e830
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-09-07 23:55:11 -0700

    Time: 110 ms (14.70%), Space: 42 MB (60.70%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
@@ -4,6 +4,27 @@
  */
 
 // Brute force
-var findMin = function(nums) {
+/*var findMin = function(nums) {
     return Math.min(...nums);
+};*/
+
+//Using Binary Search
+var findMin = function(nums) {
+    let i=0, j=nums.length-1;
+    let res=nums[0];
+    
+    while(i<=j){
+        if(nums[i]<nums[j]){
+            res=Math.min(res,nums[i]);
+        }
+        let mid=Math.trunc((i+j)/2);
+        res=Math.min(res,nums[mid]);
+        if(nums[i]<=nums[mid]){
+            i=mid+1;
+        }else{
+            j=mid-1;
+        }
+    }
+    
+    return res;
 };
