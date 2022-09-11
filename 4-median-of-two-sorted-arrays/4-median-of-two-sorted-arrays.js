8bb049f708e93d7770492471400ba1ef49d63455
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-09-10 23:21:50 -0700

    Time: 98 ms (98.18%), Space: 46 MB (94.69%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.js
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.js
@@ -19,8 +19,8 @@
     return median;
 };*/
 
-// Time Complexity O(m+n), space complexity O(1)
-var findMedianSortedArrays = function(nums1, nums2) {
+// Time Complexity O((m+n)/2), space complexity O(1)
+/*var findMedianSortedArrays = function(nums1, nums2) {
 
     let total=nums1.length+nums2.length;
     let median=0.0, half=0, i=0, j=0, ifEven=false;
@@ -64,4 +64,49 @@ var findMedianSortedArrays = function(nums1, nums2) {
     }
 
     return median;
+}*/
+var findMedianSortedArrays = function(nums1, nums2) {
+    if(nums1.length>nums2.length){
+        nums1,nums2=nums2,nums1;
+    }
+    let m=nums1.length, n= nums2.length,k, mid,kthsmallest,succ;
+    
+    if((m+n)%2==1){ // m+n Totally Odd Number of elmenets
+        k=Math.trunc((m+n)/2)+1;
+    }else{ // m+n totally even Number of elements
+        k=Math.trunc((m+n)/2);
+    }
+    const get=(arr,ind)=>{
+        if(ind<0){
+            return Number.NEGATIVE_INFINITY;
+        }else if(ind>=arr.length){
+            return Number.POSITIVE_INFINITY;
+        }else{
+            return arr[ind];
+        }
+    }
+    
+    let start=0, end=m-1;
+    while(start<=end){
+        mid=start+Math.trunc((end-start)/2);
+        if(get(nums1,mid)>=get(nums2,k-1-mid-1) && get(nums1,mid)<=get(nums2,k-1-mid)){ // Found Kth Smallest element
+            kthsmallest=get(nums1,mid);
+            succ=Math.min(get(nums1,mid+1),get(nums2,k-1-mid));
+            break;
+        }else if(get(nums1,mid)>get(nums2,k-1-mid)){// Kth smallest lies in Left Portion of the Array
+            end = mid -1;
+        }else{// Kth smallest lies in Right Portion of the Array
+            start = mid+1;
+        }
+    }
+    if(start==end+1){// If the Element Search at Nums1 is not sucessful, get the Median from Nums2
+        kthsmallest=get(nums2,k-start-1);
+        succ=Math.min(get(nums1,start),get(nums2,k-start));
+    }
+    
+    if((m+n)%2==1){ // total number of Elements are Odd
+        return kthsmallest;
+    }
+    
+    return (kthsmallest+succ)/2.0;
 }
