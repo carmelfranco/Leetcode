c197c2476d34dc17234a33faa8224d681230a252
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-09-19 00:20:17 -0700

    Time: 339 ms (31.37%), Space: 51.3 MB (8.99%) - LeetHub

---
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists) {
+    console.log(JSON.stringify(lists));
+    let r1=[],i=0;
+    while(i<lists.length){
+        let curr=lists[i];
+        while(curr){
+            console.log(curr.val);
+            r1.push(curr.val);
+            curr=curr.next;
+        }
+        i++;
+    }
+    
+    r1.sort((a,b)=>(a-b));
+    
+    let head=new ListNode(0);
+    curr=head;
+    for(let i=0;i<r1.length;i++){
+        curr.next=new ListNode(r1[i]);
+        curr=curr.next;
+    }
+    return head.next;
+};
