b8413426a5262e3261ace3b43a6d710095107108
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-09-19 23:03:45 -0700

    Time: 144 ms (70.63%), Space: 46.9 MB (90.31%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.js
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.js
@@ -9,7 +9,9 @@
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
-var mergeKLists = function(lists) {
+
+// Time Complexity O(n*k), space complexity O(n)
+/*var mergeKLists = function(lists) {
    
     let r1=[],i=0;
     while(i<lists.length){
@@ -30,4 +32,48 @@ var mergeKLists = function(lists) {
         curr=curr.next;
     }
     return head.next;
-};
+};*/
+
+// Time Complexity O(n log k), space complexity O(1)
+var mergeKLists = function(lists) {
+        if(lists===null || lists.length==0){
+            return null;
+        }
+        while(lists.length>1){
+            let result=[];
+            for(let i=0;i<lists.length;i+=2){
+                let l1=lists[i];
+                let l2=null;
+                if(i+1<lists.length){
+                    l2=lists[i+1];
+                }
+                result.push(merge2Lists(l1,l2));
+            }
+            lists=result;
+            
+        }
+        return lists[0];
+}
+
+var merge2Lists=function(l1,l2){
+    let dummy= new ListNode(0);
+    let curr=dummy;
+    while(l1 && l2){
+        if(l1.val<l2.val){
+            curr.next=l1;
+            l1=l1.next;
+        }else{
+            curr.next=l2;
+            l2=l2.next;
+        }
+        curr=curr.next;
+    }
+    if(l1){
+        curr.next=l1;
+    }
+    if(l2){
+        curr.next=l2;
+    }
+    
+    return dummy.next;
+}
