36da80dfb9d2f68abcb7b492b76eb88b5ab60f2d
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-09-22 20:35:27 -0700

    Time: 128 ms (15.74%), Space: 45.6 MB (30.62%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -11,10 +11,33 @@
  * @return {number}
  */
 // recursive DFS - T - O(n), S - O(n)
-var maxDepth = function(root) {
+/*var maxDepth = function(root) {
     if(!root){
         return 0;
     }
     // add the current element with Maximum depth of Left and right
     return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
-};
+};*/
+// iterative BFS , T- O(n), S- O(n)
+var maxDepth = function(root) {
+    if(!root){
+        return 0;
+    }
+    let q=[root];
+    let level=0;    
+    while(q.length){
+        let len=q.length;
+        level++; // Increment the level
+        while(len--){// loop until the length of the queue
+            let node=q.shift(); // get the node at index 0 and remove the element from the array
+            if(node.left){
+                q.push(node.left);// if the Left node is valid, add it to the queue
+            }
+            if(node.right){
+                q.push(node.right);// if the Right node is valid, add it to the queue
+            }
+        }
+    }
+    
+    return level;// return the level
+}
