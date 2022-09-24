9ad47137f32ae9fc2a9e317a5d6e403061cd26c8
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-09-24 00:11:57 -0700

    Time: 120 ms (46.36%), Space: 48.7 MB (29.98%) - LeetHub

---
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+// DFS inorder traversal, T-O(n), S-O(n)
+var kthSmallest = function(root, k) {
+    let result=[];
+    function dfs(node){
+        if(!node) return;
+        if(node.left) dfs(node.left);
+        result.push(node.val);
+        if(node.right) dfs(node.right);
+    }
+    dfs(root);
+    return result[k-1];
+};
