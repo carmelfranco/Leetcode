f3532ea010283dcef1829f5be505ee80a72424ca
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-09-28 22:31:17 -0700

    Time: 3099 ms (18.82%), Space: 52.6 MB (32.56%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.js
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.js
@@ -2,7 +2,7 @@
  * @param {number} k
  * @param {number[]} nums
  */
-class PriorityQueue{
+class PriorityQueue{ // Priority Queue Class
     constructor(){
         this.q=[];
         this.len=0;
@@ -22,13 +22,13 @@ class PriorityQueue{
         this.q.sort((a,b)=>(a-b));
     }
 }
-var KthLargest = function(k, nums) {
+var KthLargest = function(k, nums) { // Only store Kelements in Priority Queue
     
     this.k=k;
     this.pq = new PriorityQueue();
     for(let i=0;i<nums.length;i++){
         this.pq.add(nums[i]);
-        if(this.pq.len>k){
+        if(this.pq.len>k){ // when adding new element, check if it is beyond K value and limit
             this.pq.shift();
         }
     }    
@@ -38,11 +38,11 @@ var KthLargest = function(k, nums) {
  * @param {number} val
  * @return {number}
  */
-KthLargest.prototype.add = function(val) {
+KthLargest.prototype.add = function(val) { // Add new elements to the Priority Queue
     this.pq.add(val);
     if(this.pq.len>this.k){
-        this.pq.shift();
-        return this.pq.get(0);
+        this.pq.shift(); // when adding new element, check if it is beyond K value and limit
+        return this.pq.get(0); // return the first element in the Priority Queue
     }
     
     return this.pq.get(0);
