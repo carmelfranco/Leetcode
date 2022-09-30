7f4f9aaf917b1156b8b512f1ad71697b4fe69c84
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-09-30 01:01:06 -0700

    Time: 152 ms (82.56%), Space: 50.8 MB (60.84%) - LeetHub

---
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    k=nums.length-k;
+    function quickSelect(l,r){ // Quick Selection Algorithm
+        let pivot=nums[r], p=l; // Select the Pivot element as right element, pointer as first element
+        for(let i=l;i<r;i++){ // loop from left pointer until right pointer
+            if(nums[i]<=pivot){ // if the numbers are lesser, then swap
+                [nums[p],nums[i]]=[nums[i],nums[p]];
+                p++; // increment the pointer
+            }
+        }
+        [nums[p],nums[r]]=[nums[r],nums[p]];// swap the pivot to the right pointer
+        if(p>k){
+             return quickSelect(l,p-1); // K is in left partition
+        }else if(p<k){
+             return quickSelect(p+1,r); // K is in the right partition
+           
+        }else{// at the center
+            return nums[p]; 
+        }
+    }
+    
+    return quickSelect(0,nums.length-1);
+};
