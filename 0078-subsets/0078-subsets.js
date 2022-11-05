984a34c35a1fed9737cc6ae366b9db69d0b0ca81
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-11-05 00:01:26 -0700

    Time: 126 ms (10.79%), Space: 42.8 MB (85.64%) - LeetHub

---
+++ 0078-subsets/0078-subsets.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+    let res=[],subset=[];
+    const dfs=(i)=>{
+        if(i>=nums.length){ //Base Case
+            let cpy = [...subset];
+            res.push(cpy);
+            return;
+        }
+        // Include nums[i]
+        subset.push(nums[i]);
+        dfs(i+1);
+        //NOT to include nums[i]
+        subset.pop();
+        dfs(i+1);
+    }
+    
+    dfs(0);
+    
+    return res;
+};
