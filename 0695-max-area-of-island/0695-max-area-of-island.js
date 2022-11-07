4422bfac927d2ba29771dafdeba9657de7804879
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-11-06 20:54:40 -0800

    Time: 136 ms (55.84%), Space: 48.7 MB (25.44%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxAreaOfIsland = function(grid) {
+    let ROWS=grid.length, COLS=grid[0].length;
+    let visit = new Set(), maxarea=0;
+    
+    const dfs=(r,c)=>{
+        if(r<0 || r==ROWS || c<0 || c==COLS || grid[r][c]==0|| visit.has(r+"_"+c)){
+            return 0;
+        }
+        
+        visit.add(r+"_"+c);
+        return(1+
+                dfs(r+1,c)+
+                dfs(r-1,c)+
+                dfs(r,c+1)+
+                dfs(r,c-1)
+              );
+    }    
+    for(let r=0;r<ROWS;r++){
+        for(let c=0;c<COLS;c++){
+            maxarea=Math.max(maxarea,dfs(r,c));
+        }
+    }    
+    return maxarea;
+};
