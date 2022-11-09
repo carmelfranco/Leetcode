d0885ca1e178e4ebf5d101dfb99fdbc03e3bcc29
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-11-08 20:23:25 -0800

    Time: 106 ms (83.69%), Space: 42.2 MB (97.57%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,11 +4,31 @@
  * @return {boolean}
  */
 // Using sort method, T- O(n log n), S- O(1)
-var isAnagram = function(s, t) {
+/*var isAnagram = function(s, t) {
     s=s.split("").sort().join("");
     t=t.split("").sort().join("");
     if(s==t){
         return true;
     }
     return false;
-};
+};*/
+
+// Using Arrays T- O(n), s-O(1)
+var isAnagram = function(s, t) {
+    let arr=Array(26).fill(0);
+    if(s.length!=t.length){
+        return false;
+    }
+    for(let i=0;i<s.length;i++){
+        arr[s[i].charCodeAt()-97]++;
+    }   
+    for(let i=0;i<t.length;i++){
+        arr[t[i].charCodeAt()-97]--;
+    }    
+    for(let i=0;i<arr.length;i++){      
+       if(arr[i]!=0){
+           return false;
+       }
+    }    
+    return true;
+}
