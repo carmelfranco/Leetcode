585f4c251d55aeb3af3d50a4e65573d0a3a19d5c
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-11-11 23:47:52 -0800

    Time: 129 ms (43.01%), Space: 48 MB (24.53%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.js
+++ 0261-graph-valid-tree/0261-graph-valid-tree.js
@@ -8,14 +8,16 @@ var validTree = function(n, edges) {
     if(!n){
         return true;
     }
+    // Initialize Adj list
     let adjacency_list ={};
     for(let i=0;i<n;i++){
         adjacency_list[i]=[];
     }
+    // Populate adj list
     for(let[n1,n2] of edges){
        adjacency_list[n1].push(n2);
        adjacency_list[n2].push(n1);
-    }
+    }    
     let visit = new Set();
     function dfs(i,prev){
         if(visit.has(i)){            
@@ -26,11 +28,11 @@ var validTree = function(n, edges) {
             if(j==prev){
                 continue;
             }
-            if(!dfs(j,i)){
+            if(!dfs(j,i)){ // if it's already visited, then it's Loop
                 return false;
             }
         }
         return true;
     }   
-    return dfs(0,-1) && n==visit.size;
+    return dfs(0,-1) && n==visit.size; // set the previous value as -1 and check if the number of nodes == visit size
 };
