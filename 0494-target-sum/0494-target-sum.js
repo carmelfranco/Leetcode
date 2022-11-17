acf2810f3e7758234736bd3f3cf2b52eb2b026f8
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-11-16 23:02:16 -0800

    Time: 1939 ms (42.87%), Space: 63.9 MB (5.24%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var findTargetSumWays = function(nums, target) {
+    let dp={};
+    function backtrack(i,total){        
+        if(i==nums.length){
+            if(total==target){
+                return 1;
+            }
+            return 0;
+        }
+        if([i,total] in dp){
+            return dp[[i,total]];
+        }
+        dp[[i,total]] = backtrack(i+1,total+nums[i]) +backtrack(i+1,total-nums[i]);                         
+        return dp[[i,total]];
+    }
+    return backtrack(0,0);
+};
