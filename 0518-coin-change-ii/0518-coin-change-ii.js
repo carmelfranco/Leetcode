455efd7ce392bc6d7bf07f59c8cf85a2e4175ab3
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-11-16 23:31:34 -0800

    Time: 5224 ms (5.01%), Space: 169.2 MB (5.57%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.js
+++ 0518-coin-change-ii/0518-coin-change-ii.js
@@ -3,23 +3,24 @@
  * @param {number[]} coins
  * @return {number}
  */
-// T- O(m*n), S- O(m*n)
+ // T- O(n), s- O(n)
 var change = function(amount, coins) {
-    let dp = Array(coins.length+1).fill().map(()=>Array(amount+1).fill(0));
-    
-    // Number of ways to fill first coin is 1
-    dp[0][0] =1;
-    for(let i=1;i<dp.length;i++){
-        dp[i][0] = 1;
-        for(let j=1;j<dp[0].length;j++){
-            dp[i][j] = dp[i-1][j];
-            if(coins[i-1]<=j){
-                // Subtract the current coin from the current amount
-                // Go to the cell in the row at [current amount - current coin]
-                // Add the total number of ways in this cell to our current cell
-                dp[i][j]+=dp[i][j-coins[i-1]];
-            }
+    cache ={};
+    function dfs(i,a){
+        if(a==amount){
+            return 1;
         }
+        if(a>amount){
+            return 0;
+        }
+        if(i==coins.length){
+            return 0;
+        }
+        if([i,a] in cache){
+            return cache[[i,a]];
+        }
+        cache[[i,a]] = dfs(i,a+coins[i]) + dfs(i+1,a);
+        return cache[[i,a]];
     }
-    return dp[coins.length][amount];
+    return dfs(0,0);
 };
