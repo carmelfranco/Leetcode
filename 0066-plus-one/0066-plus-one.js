e658ede89b53a344434ce22f092c06932dc739b2
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-11-20 22:35:44 -0800

    Time: 69 ms (87.03%), Space: 41.9 MB (60.28%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -3,10 +3,25 @@
  * @return {number[]}
  */
 // T- O(1), S- O(1) (Using big Int)
-var plusOne = function(digits) {
+/*var plusOne = function(digits) {
     let str = digits.join("");
     let n = BigInt(str);
     n++;
     let str1 = n.toString().split("");    
     return str1;
-};
+};*/
+// T- O(n), S- O(1)
+var plusOne = function(digits) {
+    for(let i=digits.length-1;i>=0;i--){
+        if(digits[i]==9){
+            digits[i]=0;
+        }else{
+            digits[i]++;
+            break;
+        }
+    }
+    if(digits[0]==0){
+        digits.unshift(1);
+    }
+    return digits;
+}
