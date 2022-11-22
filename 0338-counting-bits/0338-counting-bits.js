8f45bb147cf67e9bcc3309969758ead0a5585075
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-11-21 22:17:36 -0800

    Time: 138 ms (71.70%), Space: 47.8 MB (89.84%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.js
+++ 0338-counting-bits/0338-counting-bits.js
@@ -3,7 +3,7 @@
  * @return {number[]}
  */
 // Using Bitwise operation , T- O(n^2), s- O(1)
-var countBits = function(n) {
+/*var countBits = function(n) {
     let res=[];
     for(let i=0;i<=n;i++){
         let j=i;
@@ -15,4 +15,17 @@ var countBits = function(n) {
         res.push(count);
     }
     return res;
+};*/
+
+// Using DP , T- O(n), s- O(1)
+var countBits = function(n) {
+    let dp=Array(n+1).fill(0);
+    let offset=1;
+    for(let i=1;i<=n;i++){
+        if(offset*2==i){
+            offset=i;
+        }
+        dp[i]=1+dp[i-offset];
+    }
+    return dp;
 };
