8ba9298feecce0fc888cbe72ac68117446e554d1
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-11-22 00:40:30 -0800

    Time: 70 ms (97.81%), Space: 43.3 MB (93.82%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -2,12 +2,29 @@
  * @param {number} x
  * @return {number}
  */
-// Solution 1 - Using Integer Division Operator
-var reverse = function(x) {
+// Solution 1 - Using String Operatoration
+/*var reverse = function(x) {
     const reversed = parseInt(Math.abs(x).toString().split("").reverse().join(""));
     if(reversed>2**31){
         return 0;
     }
     
     return reversed*Math.sign(x);
-};
+};*/
+
+// Solution 2 - Using Integer Division/Mod Operator  T- O(1), S- O(1)
+var reverse = function(x) {
+    let isNegative=x<0?true:false;
+    if(isNegative){
+        x=x*-1;
+    }
+    let reversed=0;
+    while(x){
+        reversed=(reversed*10)+(x%10);
+        x=Math.floor(x/10);
+    }
+    if(reversed>2**31){
+        return 0;
+    }
+    return isNegative?reversed*-1:reversed;
+}
