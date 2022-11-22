98f682e241b3813225c2cf51408df223bbdb3bb0
CarmelFranco Kulondairaj <39016490+carmelfranco@users.noreply.github.com>
2022-11-21 23:40:27 -0800

    Time: 97 ms (76.84%), Space: 44.2 MB (77.19%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,11 +3,21 @@
  * @return {number}
  */
 // Solution 1 - By sum and diff
-var missingNumber = function(nums) {
+/*var missingNumber = function(nums) {
     let sum=0;
     for(let i=0;i<=nums.length;i++){
         sum+=i;
     }
     let cur=nums.reduce((a,b)=>a+b,0);
     return sum-cur;
-};
+};*/
+
+// Solution 2 - Using XOR
+var missingNumber = function(nums) {
+    let res=nums.length;    
+    for(let i=0;i<nums.length;i++){
+        let xor=i^nums[i];
+        res^=xor;
+    }
+    return res;
+}
